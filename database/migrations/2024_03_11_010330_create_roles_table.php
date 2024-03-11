bf4bcbf4df61294c0c3e1b5aa27093fd81bd6b2c
ginoongflores <florespaul161@gmail.com>
2024-03-11 11:18:51 +0800

    ref(roles_table): remove `role_description` & insert role names

--- database/migrations/2024_03_11_010330_create_roles_table.php
+++ database/migrations/2024_03_11_010330_create_roles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -14,9 +15,14 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('role_name');
-            $table->string('role_description');
             $table->timestamps();
         });
+
+        DB::table('roles')->insert([
+            ['role_name' => 'Super Admin'],
+            ['role_name' => 'Admin'],
+            ['role_name' => 'User'],
+        ]);
     }
 
     /**
