48645d835b50e03d87be9a18316867e4e63643a4
ginoongflores <florespaul161@gmail.com>
2024-03-12 11:01:29 +0800

    temp: api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [UserController::class, 'register']);
+Route::post('login',[UserController::class, 'login']);
+Route::post('index',  [UserController::class, 'index']);
+
+Route::middleware('auth:api')->group(function () {
+
 });
