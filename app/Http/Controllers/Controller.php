f9ad7df3dedfcf374563adcdc84c536234a47e5f
ginoongflores <florespaul161@gmail.com>
2024-03-12 08:01:40 +0800

    feat(Controller): add `sendResponse` & `sendError`

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,26 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendResponse($result, $message, $success = true) {
+        $response = [
+            'success' => $success,
+            'data' => $result,
+            'message' => $message
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessage = [], $code = 404) {
+        $response = [
+            'success' => false,
+            'message' => $error
+        ];
+
+        if(!empty($errorMessage)){
+            $response['data'] = $errorMessage;
+        }
+
+        return response()->json($response, $code);
+    }
 }
