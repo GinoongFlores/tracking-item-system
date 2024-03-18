b501f397faca64f8ffff40b6665f0c054ae16aac
ginoongflores <florespaul161@gmail.com>
2024-03-18 13:07:08 +0800

    feat(/Models): add `softDeletes` on Company
    Add comment workflow on Permission model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'company_name',
     ];
+
+    // protected dates
+    protected $dates = ['deleted_at'];
+
     // users
     public function users()
     {
