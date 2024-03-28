4df2592fc0f6d21d4cc58504b2f5198bcde3942d
ginoongflores <florespaul161@gmail.com>
2024-03-28 13:53:02 +0800

    ref(UserController): modify & transfer auth functions to `AuthController`
    Modifications
    - add a null on `role_name` if no user role found upon getting the current user
    - pagination by 10
    - user role is changing upon sending a req (fix duplications of user roles on each user)

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
         return $this->sendError(['error' => 'Unauthorized'], 401);
       }
 
-        $user = User::all();
+        $user = User::latest()->paginate(10);
         return response()->json($user->toArray(), 200);
     }
 
@@ -56,10 +56,15 @@ class UserController extends Controller
         if (!$user) {
             return $this->sendError(['error' => 'No authenticated user'], 400);
         }
+
+        $role = $user->roles->first();
+        $check_role_name = $role ? $role->role_name : null;
+
         $userData = [
             'first_name' => $user->first_name,
-            // 'role' => $user->roles->first()->role_name,
+            'role' => $check_role_name,
             'email' => $user->email,
+            'is_activated' => $user->is_activated,
         ];
 
         return $this->sendResponse($userData, 'User retrieved successfully');
@@ -79,132 +84,6 @@ class UserController extends Controller
         return $this->sendResponse($user->toArray(), 'User retrieved successfully');
     }
 
-    public function login (Request $request): JsonResponse {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email|max:255',
-            'password' => 'required|min:8',
-        ]);
-
-        // show validate errors
-        if($validator->fails()) {
-            return $this->sendError(['error' => $validator->errors()], 400);
-        }
-
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $user = Auth::user();
-            $success['token'] = $user->createToken('MyApp')->accessToken;
-            $success['first_name'] = $user->first_name;
-            $token = $success['token'];
-            $profile = $success['first_name'];
-            // $role = $user->roles->first()->role_name;
-
-            return response()->json([
-                'message' => 'Welcome '. $profile,
-                'status' => 'success',
-                'token' => $token,
-            ]);
-        } else {
-            return $this->sendError('Unauthorized', ['error' => 'Unauthorized'], 401);
-        }
-    }
-
-
-    public function register (Request $request): JsonResponse {
-        $validator = Validator::make($request->all(), [
-            'first_name' => 'required|max:255',
-            'last_name' => 'required|max:255',
-            'phone' => 'numeric|unique:users',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:8',
-            'confirm_password' => 'required|same:password',
-            'company_name' => 'required|max:255',
-            // 'role' => [
-            //     'nullable',
-            //     Rule::in(['super_admin', 'admin', 'user']),
-            //     function ($attribute, $value, $fail) {
-            //         if ($value === "super_admin" && User::whereHas('roles', function($query) {
-            //             $query->where('role_name', 'super_admin');
-            //         })->count() > 0) {
-            //          $fail("The $attribute can only be one Super Admin");
-            //         //  return $this->sendError(['error' => $fail], 400);
-            //         }
-            //     },
-            // ],
-        ]);
-
-        // company name is required to users and admin, and not on super admin
-        $validator->sometimes('company_name', 'required', function ($input) {
-            return $input->role !== 'super_admin';
-        });
-
-        // show validate errors
-        if($validator->fails()) {
-            return $this->sendError(['error' => $validator->errors()], 400);
-        }
-
-        try {
-
-                // create the user
-                $input = $request->all();
-                $input['password'] = bcrypt($input['password']);
-                // $input['company_id'] = $company->id;
-
-                // start a database transaction
-                DB::beginTransaction();
-                $user = User::create($input);
-
-                // attach role
-                // $role = Role::where('role_name', $request->role)->first(); // get role
-
-                // if($role === null) {
-                //     // roll back the transaction in case of an error
-                //     DB::rollback();
-                //     return $this->sendError("Role not found", 400);
-                // }
-
-                // attach role to user
-                // $user->roles()->attach($role->id);
-
-                // attach company to user
-                if(isset($request->company_name)) {
-                    $company = Company::where('company_name', $request->company_name)->first();
-
-                    if(!$company) {
-                        // roll back in case of an error
-                        DB::rollBack();
-                        return $this->sendError(['error' => 'Company not found'], 400);
-                    }
-
-                    // attach company to user
-                    $user->company_id = $company->id;
-                    $user->save(); // save user with company
-                }
-
-                // commit the transaction
-                DB::commit();
-
-        } catch (\Exception $e) {
-            // roll back the transaction in case of an error
-            DB::rollBack();
-            //return an error response
-            return $this->sendError(['error' => $e->getMessage()], 500);
-        }
-
-        if ($user === null) {
-            return $this->sendError(['error' => 'User not created'], 500);
-        }
-
-        // token
-        $success['token'] = $user->createToken('MyApp')->accessToken;
-        $success['first_name'] = $user->first_name;
-
-        return response()->json([
-            'message' => 'Successfully registered',
-            'status' => 'success',
-            'name' => $success['first_name'],
-            'token' => $success['token'] ,
-        ]);
-    }
 
     // update/edit user details
     public function update(Request $request, $userId) : JsonResponse
@@ -283,8 +162,8 @@ class UserController extends Controller
             return $this->sendError(['error' => 'Role not found'], 400);
         }
 
-        // attach the role to the user
-        $user->roles()->attach($role->id);
+        // user role change upon request
+        $user->roles()->sync([$role->id]);
 
         return $this->sendResponse($user->toArray(), 'Role assigned successfully');
 
@@ -319,14 +198,6 @@ class UserController extends Controller
         } else {
             return $this->sendError(['error' => 'Unauthorized'], 401);
         }
-    }
-
-
-    public function logout (Request $request): JsonResponse {
-        $request->user()->token()->revoke();
-        return response()->json([
-            'message' => 'Successfully logged out'
-        ]);
     }
 }
 
